e9abb3097290dea78ead8ec6e4739b0854eff82e
dablenparty <hdavenport8902@gmail.com>
2025-01-08 20:26:18 -0500

    Create StowArgs struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct StowArgs {}
+
+fn main() -> anyhow::Result<()> {
+    let cli_args = StowArgs::parse();
+
+    #[cfg(debug_assertions)]
+    println!("{cli_args:#?}");
+
+    Ok(())
 }
