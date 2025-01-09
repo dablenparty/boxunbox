ef767edd96931272ff872166159d35ad83ed8afa
dablenparty <hdavenport8902@gmail.com>
2025-01-08 22:35:55 -0500

    Add package arg and value parser
    I needed a custom value parser to expand the ~ to the home directory

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,45 @@
+use std::{path::PathBuf, str::FromStr};
+
 use clap::Parser;
+use directories_next::UserDirs;
+
+/// Parses `&str` into a `PathBuf`. If the path begins with a `~`, it is expanded into the users
+/// home directory.
+///
+/// # Arguments
+///
+/// - `s` - String slice to parse.
+///
+/// # Errors
+///
+/// No errors should occur. It returns a `Result` because that is the required signature for a `clap`
+/// value parser, which this is.
+///
+/// # Panics
+///
+/// This function will panic if the users home directory cannot be found OR if `PathBuf::from_str`
+/// fails, although neither should ever happen.
+fn parse_and_expand_pathbuf(s: &str) -> Result<PathBuf, String> {
+    let expanded = if let Some(rest) = s.strip_prefix('~') {
+        let mut p = UserDirs::new()
+            .unwrap_or_else(|| unreachable!("Failed to locate user home directory"))
+            .home_dir()
+            .to_path_buf();
+        p.push(rest);
+        p
+    } else {
+        PathBuf::from_str(s).unwrap_or_else(|_| unreachable!("Failed to convert &str to PathBuf"))
+    };
+
+    Ok(expanded)
+}
 
 #[derive(Debug, Parser)]
-struct StowArgs {}
+struct StowArgs {
+    /// Package to stow. Can be a single file or a directory.
+    #[arg(value_parser = parse_and_expand_pathbuf)]
+    pub package: PathBuf,
+}
 
 fn main() -> anyhow::Result<()> {
     let cli_args = StowArgs::parse();
