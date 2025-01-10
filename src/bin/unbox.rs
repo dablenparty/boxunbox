d72374aee177024bb49ee96e4a9da6f8e4170863
dablenparty <hdavenport8902@gmail.com>
2025-01-09 21:58:56 -0500

    Update documentation to reflect new name

--- src/bin/unbox.rs
+++ src/bin/unbox.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 use anyhow::Context;
-use clap::Parser;
+use clap::{Parser, ValueHint};
 use directories_next::UserDirs;
 
 /// Parses `&str` into a `PathBuf`. If the path begins with a `~`, it is expanded into the users
@@ -42,29 +42,31 @@ fn parse_and_expand_pathbuf(s: &str) -> Result<PathBuf, String> {
 }
 
 #[derive(Debug, Parser, Clone)]
-struct StowArgs {
-    /// Package to stow. Can be a single file or a directory.
-    #[arg(value_parser = parse_and_expand_pathbuf)]
+struct UnboxArgs {
+    /// Package to unbox. Can be a single file or a directory.
+    #[arg(value_parser = parse_and_expand_pathbuf, value_hint = ValueHint::AnyPath)]
     pub package: PathBuf,
-    #[arg(default_value = "~", value_parser = parse_and_expand_pathbuf)]
+    /// Target directory to unbox the package to. Must be a directory.
+    #[arg(default_value = "~", value_parser = parse_and_expand_pathbuf, value_hint = ValueHint::DirPath)]
     pub target: PathBuf,
+    /// Include directories when unboxing.
     #[arg(short = 'd', long, default_value_t = false)]
     pub include_dirs: bool,
 }
 
-/// Stows a package entry in `target`. The `pkg_entry`'s file name is used for the name of the symlink.
+/// Unboxes a package entry in `target`. The `pkg_entry`'s file name is used for the name of the symlink.
 ///
 /// # Arguments
 ///
-/// - `pkg_entry` - The `DirEntry` to stow.
-/// - `target` - The `&Path` to stow in. Must be a directory.
-fn stow_package_entry(pkg_entry: &DirEntry, target: &Path) -> anyhow::Result<()> {
-    anyhow::ensure!(
-        pkg_entry.file_type().map(|ft| ft.is_dir()).unwrap_or(false),
-        "`{}`={pkg_entry:?} is not a directory!",
-        stringify!(pkg_entry),
-    );
-
+/// - `pkg_entry` - The `DirEntry` to unbox.
+/// - `target` - The `&Path` to unbox in. Must be a directory.
+///
+/// # Errors
+///
+/// An error is returned if there is an error creating the
+/// symlink.
+#[inline]
+fn unbox_package_entry(pkg_entry: &DirEntry, target: &Path) -> anyhow::Result<()> {
     let path = pkg_entry.path();
     let link_path = target.join(pkg_entry.file_name());
 
@@ -87,20 +89,18 @@ fn stow_package_entry(pkg_entry: &DirEntry, target: &Path) -> anyhow::Result<()>
 }
 
 fn main() -> anyhow::Result<()> {
-    let cli_args = StowArgs::parse();
+    let cli_args = UnboxArgs::parse();
 
     #[cfg(debug_assertions)]
     println!("{cli_args:#?}");
 
-    let StowArgs {
+    let UnboxArgs {
         include_dirs,
         ref package,
         target,
     } = cli_args;
 
-    if !package.is_dir() {
-        anyhow::bail!("{package:?} is not a directory");
-    }
+    anyhow::ensure!(!package.is_dir(), "{package:?} is not a directory");
 
     for res in package.read_dir()? {
         match res {
@@ -108,9 +108,9 @@ fn main() -> anyhow::Result<()> {
                 if !include_dirs && entry.file_type().map(|ft| ft.is_dir()).unwrap_or(false) {
                     return Ok(());
                 }
-                if let Err(error) = stow_package_entry(&entry, &target) {
+                if let Err(error) = unbox_package_entry(&entry, &target) {
                     eprintln!(
-                        "error stowing {}: {error:?}",
+                        "error unboxing {}: {error:?}",
                         entry.file_name().to_string_lossy()
                     );
                 }
