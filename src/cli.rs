87567d357bcc2e3b030fedf401a0f74db0e2f944
dablenparty <anoptimisticcouch@gmail.com>
2025-01-12 16:47:35 -0500

    Extract src/cli.rs
    I need it for the upcoming box binary

---
+++ src/cli.rs
@@ -0,0 +1,37 @@
+use std::{path::PathBuf, str::FromStr};
+
+use anyhow::Context;
+use directories_next::UserDirs;
+
+/// Parses `&str` into a `PathBuf`. If the path begins with a `~`, it is expanded into the users
+/// home directory.
+///
+/// # Arguments
+///
+/// - `s` - String slice to parse.
+///
+/// # Errors
+///
+/// An error will be returned if the path cannot be canonicalized.
+///
+/// # Panics
+///
+/// This function will panic if the users home directory cannot be found OR if `PathBuf::from_str`
+/// fails, although neither should ever happen.
+pub fn parse_and_expand_pathbuf(s: &str) -> Result<PathBuf, String> {
+    let expanded = if let Some(rest) = s.strip_prefix('~') {
+        let mut p = UserDirs::new()
+            .unwrap_or_else(|| unreachable!("Failed to locate user home directory"))
+            .home_dir()
+            .to_path_buf();
+        p.push(rest);
+        p
+    } else {
+        PathBuf::from_str(s).unwrap_or_else(|_| unreachable!("Failed to convert &str to PathBuf"))
+    };
+
+    expanded
+        .canonicalize()
+        .with_context(|| format!("failed to canonicalize: {expanded:?}"))
+        .map_err(|e| e.to_string())
+}
