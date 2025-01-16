ca09aaeec7dfff6db9612b3610e163bc2df7c705
dablenparty <hdavenport8902@gmail.com>
2025-01-16 03:00:39 -0500

    Refactor parse_and_expand to use new deps
    I replaced my simple custom implementation with shellexpand and
    path_clean and even got environment variables for free

--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,10 @@
 use std::{path::PathBuf, str::FromStr};
 
-use anyhow::Context;
 use clap::{Parser, ValueHint};
-use directories_next::UserDirs;
 use regex::Regex;
 
 /// Parses `&str` into a `PathBuf`. If the path begins with a `~`, it is expanded into the users
-/// home directory.
+/// home directory. Also parses environment variables.
 ///
 /// # Arguments
 ///
@@ -14,31 +12,20 @@ use regex::Regex;
 ///
 /// # Errors
 ///
-/// An error will be returned if the path cannot be canonicalized.
+/// An error will be returned if the path cannot be expanded or the current dir cannot be accessed.
 ///
 /// # Panics
 ///
 /// This function will panic if the users home directory cannot be found OR if `PathBuf::from_str`
 /// fails, although neither should ever happen.
 pub fn parse_and_expand_pathbuf(s: &str) -> Result<PathBuf, String> {
-    let expanded = if let Some(rest) = s.strip_prefix('~') {
-        // strip leading `/` if it exists, otherwise it will be interpreted as root when
-        // `push`ing to a `PathBuf`
-        let rest = rest.strip_prefix('/').unwrap_or(rest);
-        let mut p = UserDirs::new()
-            .unwrap_or_else(|| unreachable!("Failed to locate user home directory"))
-            .home_dir()
-            .to_path_buf();
-        p.push(rest);
-        p
-    } else {
-        PathBuf::from_str(s).unwrap_or_else(|_| unreachable!("Failed to convert &str to PathBuf"))
-    };
+    fn expand_and_clean(s: &str) -> anyhow::Result<PathBuf> {
+        let expanded = shellexpand::full(s)?;
+        let connected = std::env::current_dir()?.join(PathBuf::from_str(&expanded).unwrap());
+        Ok(path_clean::clean(connected))
+    }
 
-    expanded
-        .canonicalize()
-        .with_context(|| format!("failed to canonicalize: {expanded:?}"))
-        .map_err(|e| e.to_string())
+    expand_and_clean(s).map_err(|err| err.to_string())
 }
 
 #[derive(Debug, Parser, Clone)]
