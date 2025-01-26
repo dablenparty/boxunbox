7364a858560c5198b831f42c73c208b7168513c5
dablenparty <hdavenport8902@gmail.com>
2025-01-25 22:53:05 -0500

    Create cli_parse_pathbuf function

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,24 @@
 use std::path::PathBuf;
 
+use anyhow::Context;
 use clap::Parser;
+
+/// Parses a `&str` slice as a PathBuf, expand `~` and environment variables, and clean the path.
+fn cli_parse_pathbuf(s: &str) -> Result<PathBuf, String> {
+    fn expand_pathbuf<S: AsRef<str>>(s: S) -> anyhow::Result<PathBuf> {
+        let s = s.as_ref();
+        let expanded = shellexpand::full(s).with_context(|| format!("failed to expand {s:?}"))?;
+        let cleaned = path_clean::clean(expanded.as_ref());
+        Ok(cleaned)
+    }
+
+    expand_pathbuf(s).map_err(|err| err.to_string())
+}
 
 #[derive(Debug, Parser)]
 pub struct BoxUnboxCli {
-    #[arg(required = true)]
+    #[arg(required = true, value_parser = cli_parse_pathbuf)]
     pub package: PathBuf,
-    #[arg(short, long)]
+    #[arg(short, long, value_parser = cli_parse_pathbuf)]
     pub target: Option<PathBuf>,
 }
