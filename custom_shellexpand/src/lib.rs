40e50c8e7b93b17313cd211b11e9460ef3650de6
dablenparty <anoptimisticcouch@gmail.com>
2025-03-20 01:46:31 -0400

    refactor(shellexpand): stub expand structure

--- custom_shellexpand/src/lib.rs
+++ custom_shellexpand/src/lib.rs
@@ -1,7 +1,14 @@
 use std::path::PathBuf;
 
-fn expand(s: &str) -> PathBuf {
-    todo!("custom shellexpand")
+fn expand(s: &str) -> anyhow::Result<PathBuf> {
+    let path = PathBuf::from(s);
+    let mut comp_strs = path.components().map(|c| c.as_os_str());
+
+    while let Some(comp) = comp_strs.next() {
+        todo!("process envvars");
+    }
+
+    todo!("expand ~")
 }
 
 #[cfg(test)]
@@ -14,7 +21,7 @@ mod tests {
     fn test_expand_tilde() -> anyhow::Result<()> {
         let home = std::env::var("HOME").context("failed to get home dir")?;
         let expected = PathBuf::from(format!("{home}/path/to/file"));
-        let actual = expand("~/path/to/file");
+        let actual = expand("~/path/to/file")?;
 
         assert_eq!(expected, actual);
 
@@ -25,7 +32,7 @@ mod tests {
     fn test_expand_envvar() -> anyhow::Result<()> {
         let home = std::env::var("HOME").context("failed to get home dir")?;
         let expected = PathBuf::from(format!("{home}/some/file"));
-        let actual = expand("$HOME/some/file");
+        let actual = expand("$HOME/some/file")?;
 
         assert_eq!(expected, actual);
 
@@ -36,7 +43,7 @@ mod tests {
     fn test_expand_envvar_with_braces() -> anyhow::Result<()> {
         let home = std::env::var("HOME").context("failed to get home dir")?;
         let expected = PathBuf::from(format!("{home}/some/file"));
-        let actual = expand("${HOME}/some/file");
+        let actual = expand("${HOME}/some/file")?;
 
         assert_eq!(expected, actual);
 
