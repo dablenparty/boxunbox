a84d50e3f73e71623a1ca7fe5a93d2ecca12ffea
dablenparty <anoptimisticcouch@gmail.com>
2025-05-05 15:07:58 -0400

    test(test_utils): add FILES_TO_CREATE list

--- tests/test_utils.rs
+++ tests/test_utils.rs
@@ -1,3 +1,6 @@
+use anyhow::Context;
+use tempfile::TempDir;
+
 /// Creates a new temporary file tree for use in integration tests. Each call to this function will
 /// create a _new_ temporary directory; however, every directory will have the same structure:
 ///
@@ -13,6 +16,18 @@
 ///     ├── test.txt
 ///     └── test_ignore.txt
 /// ```
-pub fn make_tmp_tree() {
-    todo!("make_tmp_tree")
+pub fn make_tmp_tree() -> anyhow::Result<TempDir> {
+    const FILES_TO_CREATE: [&str; 6] = [
+        "src/folder1/nested1.txt",
+        "src/folder1/test_ignore2.txt",
+        "src/folder2/nested2.txt",
+        "src/folder2/nested2 again.txt",
+        "src/test.txt",
+        "src/test_ignore.txt",
+    ];
+    let temp_dir = tempfile::tempdir().context("failed to create tempdir")?;
+
+    todo!("create file structure and populate text files");
+
+    Ok(temp_dir)
 }
