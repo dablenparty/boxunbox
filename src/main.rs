fe3b1913b3623f80828a7b30196460e6d1a19f7f
dablenparty <anoptimisticcouch@gmail.com>
2025-05-15 18:45:27 -0400

    chore(main): stub out unbox function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![warn(clippy::all, clippy::pedantic)]
 
+use std::path::Path;
+
 use clap::Parser;
 use cli::{BoxUnboxCli, ColorOverride};
 
@@ -8,17 +10,34 @@ mod constants;
 mod package;
 mod utils;
 
+/// Unbox the package.
+///
+/// # Arguments
+///
+/// - `package` - Package directory to unbox.
+fn unbox(package: &Path) {
+    todo!()
+}
+
 fn main() {
     let cli = BoxUnboxCli::parse();
 
     #[cfg(debug_assertions)]
     println!("cli={cli:#?}");
 
-    let BoxUnboxCli { color_override, .. } = cli;
+    let BoxUnboxCli {
+        packages,
+        color_override,
+        ..
+    } = cli;
 
     match color_override {
         ColorOverride::Always => colored::control::set_override(true),
         ColorOverride::Auto => colored::control::unset_override(),
         ColorOverride::Never => colored::control::set_override(false),
+    }
+
+    for package in &packages {
+        unbox(package);
     }
 }
