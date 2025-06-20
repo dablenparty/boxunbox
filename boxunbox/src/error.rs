ec8088a940c79a5608822286dece1f21316e1b94
dablenparty <anoptimisticcouch@gmail.com>
2025-06-20 15:29:48 -0400

    refactor(error): reduce duplicate messages
    There's no need to display source errors with {0} or {source} since
    thiserror now properly displays them automatically as an error

--- boxunbox/src/error.rs
+++ boxunbox/src/error.rs
@@ -7,12 +7,12 @@ use crate::{
 
 #[derive(Debug, ThisError)]
 pub enum PlanningError {
-    #[error("failed to parse package config: {0}")]
+    #[error("failed to parse package config")]
     ConfigParse(#[from] ConfigRead),
     #[warn(deprecated_in_future)]
-    #[error("failed to save TOML config: {0}")]
+    #[error("failed to save TOML config")]
     ConfigWrite(#[from] ConfigWrite),
-    #[error("failed to walk package tree: {0}")]
+    #[error("failed to walk package tree")]
     Walkdir(#[from] walkdir::Error),
 }
 
@@ -20,19 +20,19 @@ pub enum PlanningError {
 pub enum UnboxError {
     #[error("cannot adopt symlink from {0}")]
     AdoptSymlink(PlannedLink),
-    #[error("failed to parse package config: {0}")]
+    #[error("failed to parse package config")]
     ConfigParse(#[from] ConfigRead),
     #[warn(deprecated_in_future)]
-    #[error("failed to save TOML config: {0}")]
+    #[error("failed to save TOML config")]
     ConfigWrite(#[from] ConfigWrite),
     #[error("dry run, not executing")]
     DryRun,
-    #[error("failed to unbox {pl}: {source}")]
+    #[error("failed to unbox {pl:?}")]
     Io {
         pl: PlannedLink,
         source: std::io::Error,
     },
-    #[error("failed to plan unboxing: {0}")]
+    #[error("failed to plan unboxing")]
     Planning(#[from] PlanningError),
     #[error("target already exists for {0}")]
     TargetAlreadyExists(PlannedLink),
--- boxunbox/src/package/error.rs
+++ boxunbox/src/package/error.rs
@@ -6,24 +6,24 @@ use thiserror::Error as ThisError;
 pub enum ConfigRead {
     #[error("config file does not exist: {0}")]
     FileNotFound(PathBuf),
-    #[error("failed to read '{path}': {source}")]
+    #[error("failed to read '{path}'")]
     Io {
         source: std::io::Error,
         path: PathBuf,
     },
-    #[error("failed to parse TOML config: {0}")]
+    #[error("failed to parse TOML config")]
     Toml(#[from] toml::de::Error),
-    #[error("failed to parse RON config: {0}")]
+    #[error("failed to parse RON config")]
     Ron(#[from] ron::error::SpannedError),
 }
 
 #[derive(Debug, ThisError)]
 pub enum ConfigWrite {
-    #[error("failed to write to '{path}': {source}")]
+    #[error("failed to write to '{path}'")]
     Io {
         source: std::io::Error,
         path: PathBuf,
     },
-    #[error("failed to serialize to TOML: {0}")]
+    #[error("failed to serialize to TOML")]
     Toml(#[from] toml::ser::Error),
 }
