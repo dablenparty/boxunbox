dca2196f14fc58c3a982a9c66a4f5d924d5ac87e
dablenparty <anoptimisticcouch@gmail.com>
2026-06-14 16:10:45 -0400

    feat!: mangen & shell complete for boxup

--- boxunbox-mangen/src/main.rs
+++ boxunbox-mangen/src/main.rs
@@ -1,23 +1,31 @@
-use std::{env, fs};
+use std::fs;
 
-use boxunbox::{cli::UnboxCli, utils::get_cargo_target};
+use boxunbox::{
+    cli::{BoxUpCli, UnboxCli},
+    utils::get_cargo_target,
+};
 use clap::CommandFactory;
 
 fn main() -> anyhow::Result<()> {
-    let cmd = UnboxCli::command();
-
-    let man = clap_mangen::Man::new(cmd);
-    let mut buffer: Vec<u8> = Vec::default();
-    man.render(&mut buffer)?;
-
     let target = get_cargo_target()?;
     let out_dir = target.join("man");
-
-    let man_name = format!("{}.1", env!("CARGO_PKG_NAME"));
     fs::create_dir_all(&out_dir)?;
-    let man_path = out_dir.join(man_name);
-    fs::write(&man_path, buffer)?;
-    println!("saved manpage to {}", man_path.display());
 
+    let cmds = vec![UnboxCli::command(), BoxUpCli::command()];
+
+    for cmd in cmds {
+        let cmd_name = cmd
+            .get_bin_name()
+            .unwrap_or_else(|| cmd.get_name())
+            .to_string();
+        let man = clap_mangen::Man::new(cmd);
+        let mut buffer: Vec<u8> = Vec::default();
+        man.render(&mut buffer)?;
+
+        let man_name = format!("{cmd_name}.1");
+        let man_path = out_dir.join(man_name);
+        fs::write(&man_path, buffer)?;
+        println!("saved manpage to {}", man_path.display());
+    }
     Ok(())
 }
--- boxunbox-shell-complete/src/main.rs
+++ boxunbox-shell-complete/src/main.rs
@@ -1,6 +1,9 @@
 use std::{fs, io::BufWriter};
 
-use boxunbox::{cli::UnboxCli, utils::get_cargo_target};
+use boxunbox::{
+    cli::{BoxUpCli, UnboxCli},
+    utils::get_cargo_target,
+};
 use clap::{CommandFactory, ValueEnum};
 use clap_complete::Shell;
 
@@ -8,19 +11,20 @@ fn main() -> anyhow::Result<()> {
     let cargo_target = get_cargo_target()?;
     let out_dir = cargo_target.join("completions");
     fs::create_dir_all(&out_dir)?;
-    let mut command = UnboxCli::command();
+    let cmds = vec![UnboxCli::command(), BoxUpCli::command()];
+    for mut command in cmds {
+        for shell in Shell::value_variants() {
+            let name = command
+                .get_bin_name()
+                .unwrap_or_else(|| command.get_name())
+                .to_string();
 
-    for shell in Shell::value_variants() {
-        let name = command
-            .get_bin_name()
-            .unwrap_or_else(|| command.get_name())
-            .to_string();
-
-        let out_path = out_dir.join(&name).with_extension(shell.to_string());
-        let fd = fs::File::create(&out_path)?;
-        let mut writer = BufWriter::new(fd);
-        println!("generating completions file {}", out_path.display());
-        clap_complete::generate(*shell, &mut command, name, &mut writer);
+            let out_path = out_dir.join(&name).with_extension(shell.to_string());
+            let fd = fs::File::create(&out_path)?;
+            let mut writer = BufWriter::new(fd);
+            println!("generating completions file {}", out_path.display());
+            clap_complete::generate(*shell, &mut command, name, &mut writer);
+        }
     }
 
     Ok(())
